1bb861f81f904a7de301372fe3b1c1adab47c4f9
jjeng08 <jjeng08@users.noreply.github.com>
2018-10-26 21:22:12 -0400

    Bad practice, but I built everything in a local file and am loading it all in now.

---
+++ java.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+	//START PAGE ANIMATIONS
+	$("#startButton").click(function () {
+		$("#triangle-down, #triangle-up, #startButton").addClass("usenand");
+		$('#page').addClass('krassInefade');
+		$("#triangle-up, #triangle-down").delay(1500).fadeOut();
+		setTimeout(function () {
+			$("#startButton").fadeOut(500);
+		}, 1);
+	});
+
+	//MAIN SECTION ANIMATIONS
+	$("#bioButton").click(function () {
+		$("#bio").slideToggle("slow");
+		$('#introBox').slideUp();
+		$('#name').fadeIn();
+		$('#title').fadeIn();
+	});
+
+	$("#galleryButton").click(function () {
+		$("#gallery").slideToggle("slow");
+		$('#introBox').slideUp();
+		$('#name').fadeIn();
+		$('#title').fadeIn();
+	});
+
+	$("#contactButton").click(function () {
+		$("#contact").slideToggle("slow");
+		$('#introBox').slideUp();
+		$('#name').fadeIn();
+		$('#title').fadeIn();
+	});
+
+	//GALLERY ANIMATIONS
+	$("#project1Cover").click(function () {
+		if ($(window).width() <820) {
+			$('#project1Content').slideToggle("slow");
+		}
+		else {
+			$("#project1Content").animate({ width: 'toggle' }, 350);
+			$("#project1Buttons").slideToggle("slow");
+
+		}
+	});
+
+	$("#project2Cover").click(function () {
+		if ($(window).width() <820) {
+			$('#project2Content').slideToggle("slow");
+		}
+		else {
+			$("#project2Content").animate({ width: 'toggle' }, 350);
+		}
+	});
+
+	$("#project3Cover").click(function () {
+		if ($(window).width() <820) {
+			$('#project3Content').slideToggle("slow");
+		}
+		else {
+			$("#project3Content").animate({ width: 'toggle' }, 350);
+		}
+	});
+});
